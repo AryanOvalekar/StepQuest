f037c55035d12bdc4ea147f05be62f28e9e4e89e
AryanOvalekar <aryanovalekar@gmail.com>
2023-01-13 23:11:27 -0500

    Add Registration Backend

--- frontend/src/Components/LoginRegister/Register.js
+++ frontend/src/Components/LoginRegister/Register.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import { useState } from 'react';
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {  
-
+    let nav = useNavigate();
     const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
     const [username, setUsername] = useState("");  
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const submitLogin = (event) => {
-        console.log("Logged in!")
-
-        //ADD AXIOS METHOD
+    const submitRegister = (event) => {
+        event.preventDefault();
+        
+        axios.post("http://localhost:3001/register", {
+            username:username,
+            password:password,
+            email:email,
+        })
+        .then(response => { 
+            if(response.data.length!=0){
+                nav("/login")
+            }
+            else{
+                nav("/registration-failed")
+            }});
     }
 
     return (
-        <div class="login-form" onSubmit={submitLogin}>
+        <div class="login-form" onSubmit={submitRegister}>
             <form>
                 <h1>SIGN UP</h1>
                     <input className = "login-element" type='username' placeholder = 'Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
