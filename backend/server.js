8695a6d6f3fb6224ad1907d508b23c79a64f4a79
AryanOvalekar <aryanovalekar@gmail.com>
2023-01-13 21:51:53 -0500

    Add Database Login

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,32 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+app.use(express.json());    
+app.use(cors())
 
 const UserModel = require("./models/User");
 
-const username = "Aryan"
-const email = "aryanovalekar@gmail.com"
-const password = "bruh"
 
-const user = new UserModel({username: username, email: email, password: password})
 
-user.save();
+//const user = new UserModel({username: username, email: email, password: password})
+//user.save();
+
+app.post("/login", async (req, res) =>{  //check if an account exists
+    const email = req.body.email;
+    const password = req.body.password;
+    UserModel.find({email: email, password: password}, (err, result)=>{
+        
+        if(err){    
+            res.send(err);
+        }
+        if(result){
+        res.send(result);}
+        else{   
+            res.send("Account Not Found")
+        }
+    })
+})
 
 app.listen(3001, ()=> {
-    //  console.log('Server up and running on 3001');
+    console.log("LOADED")
   });
