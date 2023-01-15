47520fd54ddd714a11f36ce0dbbdf65a69e30e6f
AryanOvalekar <aryanovalekar@gmail.com>
2023-01-14 21:51:17 -0500

    add plaque css

--- frontend/src/Components/TrophyRoom/Plaque.js
+++ frontend/src/Components/TrophyRoom/Plaque.js
@@ -2,9 +2,14 @@ import React from 'react'
 import "./Plaque.css"
 
 const Plaque = (props) => {
+  const gold = {background:"linear-gradient(135deg, rgba(246,217,94,1) 0%, rgba(237,144,88,1) 100%)"}
+  const silver = {background:"linear-gradient(135deg, rgba(213,213,213,1) 0%, rgba(122,122,122,1) 100%)"}
+  const bronze = {background:"linear-gradient(135deg, rgba(221,136,55,1) 0%, rgba(103,64,25,1) 100%)"}
+  const normal = {background:"rgb(231,231,231)"}
+
   return (
     <div>
-        <div className='plaque'>
+        <div className='plaque' style={gold}>
             <img></img>
         </div>
     </div>
